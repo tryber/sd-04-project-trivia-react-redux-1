713002e8fbc305c7d300ae1b6d17d460bce290d2
Gregor H Andrade <hammerschmittgregor2@gmail.com>
2020-07-15 18:53:45 -0300

    fetch maledito

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import logo from './trivia.png';
 import './App.css';
+import { getQuestions, getToken } from './service/index';
 
-export default function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          SUA VEZ
-        </p>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>SUA VEZ</p>
+        </header>
+      </div>
+    );
+  }
 }
---
+++ src/service/index.js
@@ -0,0 +1,8 @@
+export const getToken = () =>
+  fetch('https://opentdb.com/api_token.php?command=request')
+    .then((response) => response.json())
+    .then((data) =>
+      fetch(`https://opentdb.com/api.php?amount=5&token=${data.token}`)
+        .then((response) => response.json())
+        .then((data) => data.results),
+    );
