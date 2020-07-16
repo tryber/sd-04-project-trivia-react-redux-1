b48fc8f6912ea4ca38b2be5512224df5b266a239
Ronald Lima <ronaldazevedolima@gmail.com>
2020-07-16 14:22:40 -0300

    trocando router de lugar

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
@@ -9,9 +8,7 @@ import store from './redux/store';
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <Route path="/" component={App} />
-    </Router>
+    <App/>
   </Provider>,
   document.getElementById('root'),
 );
