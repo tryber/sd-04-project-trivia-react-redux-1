f614804359ddb786587533032e55620e01aac6c5
Ronald Lima <ronaldazevedolima@gmail.com>
2020-07-18 15:11:35 -0300

    cc2

--- src/pages/Configuracao.js
+++ src/pages/Configuracao.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import { getCategorys } from '../service';
+// import { getCategorys } from '../service';
 
 // const categorys = getCategorys().then((cat) => cat.map(e => <option>{e.name}</option>))
-  
+
 class Configuracao extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       categorias: [],
-    }
+    };
     // this.sel = this.sel.bind(this);
   }
 
@@ -20,19 +20,18 @@ class Configuracao extends React.Component {
 
   // sel = () => {
   //   const { categorias } = this.state;
-  //   return !categorias ? <option>loading</option> : categorias.map((cat) => <option>{cat}</option>)
-  // }  
-
+  //   return !categorias ? <option>loading</option> : 
+  //categorias.map((cat) => <option>{cat}</option>)
+  // }
   render() {
     // const { categorias } = this.state
-  return (
-    <form>
-      <h2 data-testid="settings-title">Configurações</h2>
-      <select>
-      {/* {this.sel()}  */}
-      </select>
-    </form>
-  );
-}}
+    return (
+      <form>
+        <h2 data-testid="settings-title">Configurações</h2>
+        <select>{/* {this.sel()}  */}</select>
+      </form>
+    );
+  }
+}
 
 export default Configuracao;
