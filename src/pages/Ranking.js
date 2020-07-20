08adfc5e0843e61148cd732da85b935b106c3fa0
André Ferreira <andrefpires1@gmail.com>
2020-07-20 13:05:41 -0300

    listaranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import HomePage from '/.HomePage'
+import HomePage from './HomePage'
+import { Link } from 'react-router-dom';
+import GameHeader from '../components/GameHeader'
 
-class Ranking extends React.Component {
-  return () {
-    <div>
+const Ranking = ({ name, score }) => {
+
+  const players = {};
+  
+  state.reduce((players, element => {return players + element}).sort((a, b) => {return b - a}));
+
+  return ( 
+    <div className="App">
+      <GameHeader />
+      <div className="">
+      <img
+        data-testid="header-profile-picture"
+        src={`https://www.gravatar.com/avatar/${md5(props.email).toString()}`}
+        alt="img jogador"
+      />
+      <h2 data-testid="player-name-${index}"> {name} - </h2>
+      <h2 data-testid="player-score-${index}"> {score} pontos </h2>
+      </div>
+      <Link data-testid="btn-go-home" to="/HomePage">Go home</Link>
       <Button onClick = {() => {HomePage}} data-testid="btn-go-home">Go home</Button>
     </div>
-  }
+  )
 }
 
 export default Ranking;
