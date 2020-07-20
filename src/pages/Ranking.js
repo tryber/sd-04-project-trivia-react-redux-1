e1660a0fb6a6af8589a9bb4e93ed6d253e619557
André Ferreira <andrefpires1@gmail.com>
2020-07-20 18:03:53 -0300

    update

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -3,25 +3,22 @@ import { Link } from 'react-router-dom';
 
 const Ranking = () => {
   const players = JSON.parse(localStorage.getItem('ranking'));
-  players.sort((a, b) => {return b.score - a.score});
-  
-  const player = () => {
-    return players.map((element, index) => {
-      return(
-        <li key={index}>
-          <img src={element.picture} alt='foto-perfil' />
+  players.sort((a, b) => b.score - a.score);
+  const player = () =>
+    players.map((element, index) =>
+        <li key={`${element.name}-${index}`}>
+          <img src={element.picture} alt="foto-perfil" />
           <p data-testid={`player-name-${index}`}>Jogador: {element.name}</p>
           <p data-testid={`player-score-${index}`}>Pontos: {element.score}</p>
         </li>
-      )
-    })
-  }
+    );
+
   return (
     <div>
       <ol>{player()}</ol>
       <Link data-testid="btn-go-home" to="/">Go home</Link>
     </div>
-  )
-}
+  );
+};
 
 export default Ranking;
